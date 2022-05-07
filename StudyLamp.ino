04bebb69bcc1877d5d542718551b701f9356c34a
ehcrae <48123279+ehcrae@users.noreply.github.com>
2022-05-07 21:07:23 +1000

    Refactored fading
    Created fading function
    - ability to change fading pin
    - choose value to fade to (0, 255)

--- StudyLamp.ino
+++ StudyLamp.ino
@@ -1,39 +1,32 @@
 #define whitePin 9
 #define redPin 10
 
+void fade(int pinVal, int endVal) {
+    if (endVal == 255) {
+        for (int i = 0; i <= 255; i++) {
+            analogWrite(pinVal, i);
+            delay(10);
+        }
+    }
+    else {
+        for (int i = 255; i >= 0; i--) {
+            analogWrite(pinVal, i);
+            delay(10);
+        }
+    }
+}
+
 void setup() {
-    pinMode(whitePin, OUTPUT);
-    pinMode(redPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-
-    for (int i = 0; i <= 255; i++) {
-        analogWrite(redPin, i);
-        delay(10);
-    }
-
+    fade(whitePin, 255);
     delay(30000);
-
-    for (int i = 255; i >= 0; i--) {
-        analogWrite(redPin, i);
-        delay(10);
-    }
-
+    fade(whitePin, 0);
     delay(100);
-
-    for (int i = 0; i <= 255; i++) {
-        analogWrite(whitePin, i);
-        delay(10);
-    }
-
+    fade(redPin, 255);
     delay(6000);
-
-    for (int i = 255; i >= 0; i--) {
-        analogWrite(whitePin, i);
-        delay(10);
-    }
-
+    fade(redPin, 0);
     delay(100);
-    
 }
