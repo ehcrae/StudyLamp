3576be4a9354287347d5bf10c014a29bf22c9d9e
ehcrae <48123279+ehcrae@users.noreply.github.com>
2022-05-07 20:55:12 +1000

    Pin values, better delay() values

--- StudyLamp.ino
+++ StudyLamp.ino
@@ -1,19 +1,39 @@
-#define ledPin 3
+#define whitePin 9
+#define redPin 10
 
 void setup() {
-    pinMode(ledPin, OUTPUT)
+    pinMode(whitePin, OUTPUT);
+    pinMode(redPin, OUTPUT);
 }
 
 void loop() {
-    for (i = 0, i <= 255; i++) {
-        analogWrite(ledPin, i);
+
+    for (int i = 0; i <= 255; i++) {
+        analogWrite(redPin, i);
         delay(10);
     }
-    
+
+    delay(30000);
+
+    for (int i = 255; i >= 0; i--) {
+        analogWrite(redPin, i);
+        delay(10);
+    }
+
+    delay(100);
+
+    for (int i = 0; i <= 255; i++) {
+        analogWrite(whitePin, i);
+        delay(10);
+    }
+
+    delay(6000);
+
+    for (int i = 255; i >= 0; i--) {
+        analogWrite(whitePin, i);
+        delay(10);
+    }
+
     delay(100);
     
-    for (i = 255; i <= 0; i--) {
-        analogWrite(ledPin, i);
-        delay(10);
-    }
 }
