234ce3fb5f95cea1d11955e2a4a73463f372366b
Archie Collison <archie.collison@gmail.com>
2022-08-20 17:43:26 +1000

    Added terminal output

--- StudyLamp.ino
+++ StudyLamp.ino
@@ -1,6 +1,6 @@
 #define PomodoroTime 25
 #define BreakTime 5
-// change your times above this line :) (the values are in minutes)
+// change your times above this line (in minutes)
 #define stripPin 5
 #define buttonPin 6
 
@@ -14,8 +14,10 @@ void loop() {
     for (int i = 0; i <= PomodoroTime*60000; i++) {
         if (digitalRead(buttonPin) == HIGH && digitalRead(stripPin) == LOW) {
             digitalWrite(stripPin, HIGH);
+            Serial.print("checked for light, status: " + digitalRead(buttonPin));
             } else if (digitalRead(buttonPin) == HIGH && digitalRead(stripPin) == HIGH) {
                 digitalWrite(stripPin, LOW);
+                Serial.print("checked for light, status: " + digitalRead(buttonPin));
                 }
     delay(200);
     for (int i = 0; i <= BreakTime*60000; i++) {
