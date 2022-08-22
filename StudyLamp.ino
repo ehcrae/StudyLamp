cbe335ad102953d90ec59044c4e16d3dec871c0c
Archie Collison <archie.collison@gmail.com>
2022-08-22 13:00:37 +1000

    Added support for switch

--- StudyLamp.ino
+++ StudyLamp.ino
@@ -2,30 +2,21 @@
 #define BreakTime 5
 // add above this line (in minutes)
 #define stripPin 5
-#define buttonPin 6
+#define switchPin 6
 
 void setup() {
   pinMode(stripPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(switchPin, INPUT);
   digitalWrite(stripPin, HIGH);
 }
 
 void loop() {
-    for (int i = 0; i <= PomodoroTime*60000; i++) {
-        if (digitalRead(buttonPin) == HIGH && digitalRead(stripPin) == LOW) {
-            digitalWrite(stripPin, HIGH);
-            delay(100);
-            } else if (digitalRead(buttonPin) == HIGH && digitalRead(stripPin) == HIGH) {
-                digitalWrite(stripPin, LOW);
-                delay(100);
-                }
-    }
-    delay(200);
-    for (int i = 0; i <= BreakTime*60000; i++) {
-        if (digitalRead(buttonPin) == HIGH && digitalRead(stripPin) == LOW) {
-            digitalWrite(stripPin, HIGH);
-            } else if (digitalRead(buttonPin) == HIGH && digitalRead(stripPin) == HIGH) {
-                digitalWrite(stripPin, LOW);
-                }
+    while (digitalRead(switchPin) == HIGH)
+    {
+        digitalWrite(stripPin, HIGH);
+        delay(PomodoroTime*60000);
+        digitalWrite(stripPin, LOW);
+        delay(200);
+        digitalWrite(stripPin, HIGH);
     }
 }
