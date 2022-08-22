e05d744983bd6d7241b6e6fed8c42e443a897a6f
Archie Collison <archie.collison@gmail.com>
2022-08-22 19:04:16 +1000

    Completed support for toggle switch

--- StudyLamp.ino
+++ StudyLamp.ino
@@ -1,22 +1,35 @@
-#define PomodoroTime 25
-#define BreakTime 5
-// add above this line (in minutes)
+unsigned long StudyMinutes = 25;
+unsigned long BreakMinutes = 5;
+unsigned long DelayMillis = 1000;
+
+// adjust your timing above this line (in minutes)
+
 #define stripPin 5
-#define switchPin 6
+#define buttonPin 6
 
 void setup() {
   pinMode(stripPin, OUTPUT);
-  pinMode(switchPin, INPUT);
-  digitalWrite(stripPin, HIGH);
+  pinMode(buttonPin, INPUT);
 }
 
+unsigned long studyMillis = 0;
+unsigned long breakMillis = 0;
+
 void loop() {
-    while (digitalRead(switchPin) == HIGH)
-    {
-        digitalWrite(stripPin, HIGH);
-        delay(PomodoroTime*60000);
-        digitalWrite(stripPin, LOW);
-        delay(200);
-        digitalWrite(stripPin, HIGH);
-    }
+  unsigned long previousMillis = millis();
+
+  while (studyMillis < (StudyMinutes*60000 + previousMillis) && digitalRead(buttonPin) == HIGH) {
+    studyMillis = millis();
+    digitalWrite(stripPin, HIGH);
+  }
+
+  digitalWrite(stripPin, LOW);
+  delay(DelayMillis);
+
+  while (breakMillis < (BreakMinutes*60000 + studyMillis + previousMillis) && digitalRead(buttonPin) == HIGH) {
+    breakMillis = millis();
+    digitalWrite(stripPin, HIGH);
+  }
+  studyMillis = 0;
+  breakMillis = 0;
 }
