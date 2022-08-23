c68dbb4d8a399b1d47749f3052178eaf96fd5d57
Archie Collison <archie.collison@gmail.com>
2022-08-23 22:43:10 +1000

    Added more detail to comments

--- StudyLamp.ino
+++ StudyLamp.ino
@@ -1,16 +1,15 @@
-unsigned long StudyMinutes = 25;
-unsigned long BreakMinutes = 5;
+unsigned long StudyMinutes = 25; // minutes
+unsigned long BreakMinutes = 5; // minutes
+unsigned long DelayMillis = 1000; // milliseconds
 
-// adjust your timing above this line (in minutes)
-
-unsigned long DelayMillis = 1000;
+// adjust the timing of your pomodoro sessions with the values above!
 
 #define stripPin 5
-#define buttonPin 6
+#define rockerPin 6
 
 void setup() {
   pinMode(stripPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(rockerPin, INPUT);
 }
 
 unsigned long studyMillis = 0;
@@ -19,17 +18,17 @@ unsigned long breakMillis = 0;
 void loop() {
   unsigned long previousMillis = millis();
 
-  while (studyMillis < (StudyMinutes*60000 + previousMillis) && digitalRead(buttonPin) == HIGH) {
+  while (studyMillis < (StudyMinutes*60000 + previousMillis) && digitalRead(rockerPin) == HIGH) {
     studyMillis = millis();
     digitalWrite(stripPin, HIGH);
   }
 
-  digitalWrite(stripPin, LOW);
+  digitalWrite(rockerPin, LOW);
   delay(DelayMillis);
 
-  while (breakMillis < (BreakMinutes*60000 + studyMillis + previousMillis) && digitalRead(buttonPin) == HIGH) {
+  while (breakMillis < (BreakMinutes*60000 + studyMillis + previousMillis) && digitalRead(rockerPin) == HIGH) {
     breakMillis = millis();
-    digitalWrite(stripPin, HIGH);
+    digitalWrite(rockerPin, HIGH);
   }
   studyMillis = 0;
   breakMillis = 0;
